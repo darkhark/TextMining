dbad6ccb22378b89e32000585ec875c4f5578e9f
darkhark <joshua.harkness32@gmail.com>
2020-09-21 23:31:16 -0400

    Added lemmatization for verbs for question 3

--- src/main/python/Assignment01/MobyTokens.py
+++ src/main/python/Assignment01/MobyTokens.py
@@ -6,7 +6,7 @@ def getMobyTxtLocation():
     return str(Path(__file__).parent.parent.parent.parent.parent / "data") + "/moby.txt"
 
 
-def getTokens(fileAsString: str):
+def getAllTokensSorted(fileAsString: str):
     """
     The tokens will be created using the nltk
     library.
@@ -21,11 +21,12 @@ def getTokens(fileAsString: str):
 def wordAndPunctuationTokenCounter(tokens) -> list:
     """
     Counts tokens in a document.
+
     @param tokens: The tokens from
     """
     print("Total number of tokens: " + str(len(tokens)))
     uniqueTokenList = nltk.FreqDist(tokens)
-    print(len(uniqueTokenList))
+    print("Unique number of tokens: " + str(len(uniqueTokenList)))
 
 
 def getFileAsRawString(filePath: str) -> str:
@@ -34,6 +35,32 @@ def getFileAsRawString(filePath: str) -> str:
     return fileAsString
 
 
+def getVerbLemmatizations(tokens) -> list:
+    """
+    Takes in a list of tokens and finds all the verbs then
+    lemmatizes them.
+
+    @param tokens: A list of tokens
+    @return: The list of tokens after lemmatization.
+    """
+    nltk.download('wordnet')
+    lemmatizer = nltk.WordNetLemmatizer()
+    lemmaTokens = nltk.pos_tag(tokens)
+    verbArray = ['VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+    results = []
+    for token in lemmaTokens:
+        if token[1] in verbArray:
+            resultingToken = lemmatizer.lemmatize(token[0], pos='v')
+        else:
+            resultingToken = token[0]
+        results.append(resultingToken)
+    return results
+
+
 fileLocation = getMobyTxtLocation()
-mobyTokens = getTokens(getFileAsRawString(fileLocation))
+stringFile = getFileAsRawString(fileLocation)
+mobyTokens = getAllTokensSorted(stringFile)
 wordAndPunctuationTokenCounter(mobyTokens)
+verbLemmatizedTokens = getVerbLemmatizations(mobyTokens)
+wordAndPunctuationTokenCounter(verbLemmatizedTokens)
+
